5d493d3c823149f51cedfbbf4980ee6682eeb651
Rajat <163384006+RajatSaini04@users.noreply.github.com>
2025-11-30 14:16:51 +0000

    fix analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import './App.css'
 import ChessGame from './ChessGame'
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <ChessGame />
+      <Analytics />
     </>
   )
 }
